aac9894898c34c117650644a4ab507f94a28d79a
Bartosz Jarocki <jarocki.bartek@gmail.com>
2022-07-21 14:59:15 +0200

    No round corners on mobile

--- components/card.tsx
+++ components/card.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Card: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
-    <div className="bg-white rounded-lg max-w-xl w-full flex flex-col items-center justify-center divide-y shadow-xl">
+    <div className="bg-white sm:rounded-lg max-w-xl w-full flex flex-col items-center justify-center divide-y shadow-xl">
       {children}
     </div>
   );
