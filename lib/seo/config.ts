af7a21b0b59708b87afa7dc04975407a50f73adf
Bartosz Jarocki <jarocki.bartek@gmail.com>
2022-07-29 09:58:53 +0200

    Updated meta title and description

--- lib/seo/config.ts
+++ lib/seo/config.ts
@@ -1,5 +1,5 @@
 export const SEO_CONFIG = {
-  title: "Parabol | Icebreakers",
-  description: "Generate random meeting icebreakers",
+  title: "Random Icebreaker Generator | Parabol",
+  description: "Find the perfect icebreaker question for your next meeting or event. All icebreaker questions have been verified to ensure they are inclusive for diverse groups.",
   canonical: process.env.PUBLIC_URL,
 };
