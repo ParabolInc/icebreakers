d7e93187cd506619c4689a61255d7c966facc622
Bartosz Jarocki <jarocki.bartek@gmail.com>
2022-08-20 18:44:22 +0200

    Extend static images cache (#11)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
+
+const ONE_YEAR_SECONDS = 60 * 60 * 24 * 365;
+const CACHE_CONTROL_HEADER = {
+  key: "Cache-Control",
+  value: `public, max-age=${ONE_YEAR_SECONDS}, immutable`,
+};
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  headers: async () => {
+    return [
+      {
+        source: "/img/:path*",
+        headers: [CACHE_CONTROL_HEADER],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
