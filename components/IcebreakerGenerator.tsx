73b74ae1802bfb5aba335bfd991af62e739608bd
Bartosz Jarocki <jarocki.bartek@gmail.com>
2022-10-06 12:41:02 +0200

    code cleanup

--- components/IcebreakerGenerator.tsx
+++ components/IcebreakerGenerator.tsx
@@ -18,7 +18,7 @@ export const IcebreakerGenerator = ({
   return (
     <div className="flex w-full flex-col items-center justify-center divide-y">
       <div className="flex items-center justify-center p-5 sm:p-6 sm:pt-7">
-        <a href=${PARABOL_URI}>
+        <a href={PARABOL_URI}>
           <Logo className="h-6 w-auto sm:h-8" />
         </a>
       </div>
