784e8e78758b6e827280e54682c107d8f7c41283
Bartosz Jarocki <jarocki.bartek@gmail.com>
2022-11-23 15:51:03 +0100

    Added vercel analytics (#16)
    * Added vercel analytics
    
    * Code cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { IBM_Plex_Sans } from "@next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const ibmPlexSans = IBM_Plex_Sans({
   weight: ["400", "500", "600", "700"],
@@ -16,6 +17,7 @@ function IcebreakersApp({ Component, pageProps }: AppProps) {
         }
       `}</style>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
